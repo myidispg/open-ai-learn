49001add201b6422c7ff9eed2415da59045976a3
myidispg <myidispg@gmail.com>
2018-10-30 18:35:13 +0530

    Working on self implementation of FrozenLake

--- frozen-lake-self.py
+++ frozen-lake-self.py
@@ -17,4 +17,33 @@ action_size = env.action_space.n
 # States are the rows of the q table
 state_size = env.observation_space.n
 
-qtable = np.zeros((state_size, action_space))
+qtable = np.zeros((state_size, action_size))
+
+# Some parameters
+total_episode = 15000
+learning_rate = 0.8
+max_steps = 99
+gamma = 0.95
+
+# Exploration parameters
+epsilon = 1.0
+max_epsilon = 1.0
+min_epsilon = 1.0
+
+decay_rate = 0.005 # Exponential decay rate for exploration prob.
+
+# List of rewards
+rewards = []
+
+# Loop to play 15000 times.
+for episode in range(total_episodes):
+    state = env.reset()
+    step = 0
+    done = False
+    total_rewards = 0
+    
+    # Play each episode a max of 99 times.
+    for step in range(max_steps):
+        
+    
+    
