38088bac78b54e56a3ce9cf60d50230a5dd0c1b3
myidispg <myidispg@gmail.com>
2018-10-31 17:54:07 +0530

    Working on self implementation of Taxi-v2

--- taxi_self.py
+++ taxi_self.py
@@ -13,7 +13,34 @@ import random
 env = gym.make('Taxi-v2')
 env.render()
 
-action = env.action_space.n # The number of columns
-states = env.observation_space.n # The number of rows.
+action_size = env.action_space.n # The number of columns
+state_size = env.observation_space.n # The number of rows.
+
+qtable = np.zeros((state_size, action_size))
 
 # Define some parameters.
+total_episodes = 5000
+total_test_episodes = 100
+max_steps = 99
+
+learning_rate = 0.7
+gamma = 0.618 # Rate at which future rewards are discounted.
+
+# Exploration parameters
+epsilon = 1.0
+max_epsilon = 1.0
+min_epsilon = 0.1
+decay_rate = 0.01
+
+# List of rewards
+for episode in range(total_episodes):
+    state = env.reset()
+    step = 0
+    done = False
+    
+    for step in range(max_steps):
+        
+        exploration_exploitation_tradeoff = random.uniform(0, 1)
+        
+        if exploration_exploitation_tradeoff > epsilon: # Exploit
+            
