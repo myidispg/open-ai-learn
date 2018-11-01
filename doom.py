c0a176cf77ecb7b3748bda4d49d0e5b86ebd9707
myidispg <myidispg@gmail.com>
2018-11-01 14:04:14 +0530

    Started work on DOOMgit add --all!

--- doom.py
+++ doom.py
@@ -68,3 +68,72 @@ def test_environent():
     game.close()
     
 game, possible_actions = create_environment()
+
+"""
+    preprocess_frame:
+    Take a frame.
+    Resize it.
+        __________________
+        |                 |
+        |                 |
+        |                 |
+        |                 |
+        |_________________|
+        
+        to
+        _____________
+        |            |
+        |            |
+        |            |
+        |____________|
+    Normalize it.
+    
+    return preprocessed_frame
+    
+    """
+def preprocess_frame(frame):
+    # Greyscale frame already done in our vizdoom config
+    # x = np.mean(frame,-1)
+    
+    # Crop the screen (remove the roof because it contains no information)
+    cropped_frame = frame[30:-10,30:-30]
+    
+    # Normalize Pixel Values
+    normalized_frame = cropped_frame/255.0
+    
+    # Resize
+    preprocessed_frame = transform.resize(normalized_frame, [84,84])
+    
+    return preprocessed_frame
+
+stack_size = 4 # Stack 4 frames
+
+# Inititalize deque with zero images one array for each image
+stacked_frames = deque([np.zeros((84,84), dtype=int)] for i in range(stack_size)], max_len = 4)
+    
+def stacked_frames(stacked_frames, state, is_new_episode):
+    # Preprocessed frames
+    frame = preprocess_frame(state)
+    
+    if is_new_episode:
+        # CLear out the stacked frames
+        stacked_frames = deque([np.zeros((84,84), dtype=np.int) for i in range(stack_size)], maxlen=4)
+        
+        # Because we're in a new episode, copy the same frame 4x
+        stacked_frames.append(frame)
+        stacked_frames.append(frame)
+        stacked_frames.append(frame)
+        stacked_frames.append(frame)
+        
+        # Stack the frames
+        stacked_state = np.stack(stacked_frames, axis=2)
+    
+    else:
+        # Append frame to deque, automatically removes the oldest frame
+        stacked_frames.append(frame)
+
+        # Build the stacked state (first dimension specifies different frames)
+        stacked_state = np.stack(stacked_frames, axis=2) 
+    
+    return stacked_state, stacked_frames
+        
