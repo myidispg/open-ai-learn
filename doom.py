e5c39436a0094bae3bf581ec6649e0eec3ceef6c
myidispg <myidispg@gmail.com>
2018-11-07 21:43:43 +0530

    Working on DOOMgit add --all!

--- doom.py
+++ doom.py
@@ -116,7 +116,7 @@ def stack_frames(stacked_frames, state, is_new_episode):
     frame = preprocess_frame(state)
     
     if is_new_episode:
-        # CLear out the stacked frames
+        # Clear out the stacked frames
         stacked_frames = deque([np.zeros((84,84), dtype=np.int) for i in range(stack_size)], maxlen=4)
         
         # Because we're in a new episode, copy the same frame 4x
@@ -139,7 +139,7 @@ def stack_frames(stacked_frames, state, is_new_episode):
 
 ### MODEL HYPERPARAMETERS
 state_size = [84,84,4]      # Our input is a stack of 4 frames hence 84x84x4 (Width, height, channels) 
-action_size = game.get_available_buttons_size()              # 3 possible actions: left, right, shoot
+action_size = game.get_available_buttons_size() # 3 possible actions: left, right, shoot
 learning_rate =  0.0002      # Alpha (aka learning rate)
 
 ### TRAINING HYPERPARAMETERS
@@ -165,4 +165,99 @@ training = True
 ## TURN THIS TO TRUE IF YOU WANT TO RENDER THE ENVIRONMENT
 episode_render = False
 
+class DQNetowrk:
+    
+    def __init__(self, state_size, action_size, learning_rate, name='DQNetwork'):
+        self.state_size = state_size
+        self.action_size = action_size
+        self.learning_rate = learning_rate
+        
+        with tf.variable_scope(name):
+            # We create the placeholders
+            # *state_size means that we take each elements of state_size in tuple hence is like if we wrote
+            # [None, 84, 84, 4]
+            self.inputs_ = tf.placeholder(tf.float32, [None, *state_size], name="inputs")
+            self.actions_ = tf.placeholder(tf.float32, [None, 3], name="actions_")
+            # Remember that target_Q is the R(s,a) + ymax Qhat(s', a')
+            self.target_Q = tf.placeholder(tf.float32, [None], name="target")
+            """
+            First convnet:
+            CNN
+            BatchNormalization
+            ELU
+            """
+            # Input is 84x84x4
+            self.conv1 = tf.layers.conv2d(inputs = self.inputs_, filters=32, kernel_size=[8,8], strides=[4,4],
+                        padding='VALID', kernel_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
+                        name="conv1")
+            self.conv1_batchnorm = tf.layers.batch_normalization(self.conv1,
+                                                                 training = True,
+                                                                 epsilon = 1e-5,
+                                                                 name='batch_norm1')
+            self.conv1_out = tf.nn.elu(self.conv1_batchnorm, name="conv1_out")
+            
+            """
+            Second convnet:
+            CNN
+            BatchNormalization
+            ELU
+            """
+            self.conv2 = tf.layers.conv2d(inputs=self.conv1_out, filters=64, kernel_size=[4,4],
+                                          strides=[2,2], padding='VALID', 
+                                          kernel_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
+                                          name='conv2')
+            self.conv2_batchnorm = tf.layers.batch_normalization(self.conv2, 
+                                                                 training=True,
+                                                                 epsilon=1e-5,
+                                                                 name='batch_norm2')
+            self.conv2_out = tf.nn.elu(self.conv2_batchnorm, name='conv2_out') ## --> [9, 9, 64]
+            """
+            Third convnet:
+            CNN
+            BatchNormalization
+            ELU
+            """
+            self.conv3 = tf.layers.conv2d(inputs = self.conv2_out,
+                                 filters = 128,
+                                 kernel_size = [4,4],
+                                 strides = [2,2],
+                                 padding = "VALID",
+                                kernel_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
+                                 name = "conv3")
+            self.conv3_batchnorm = tf.layers.batch_normalization(self.conv3,
+                                                   training = True,
+                                                   epsilon = 1e-5,
+                                                     name = 'batch_norm3')
+            self.conv3_out = tf.nn.elu(self.conv3_batchnorm, name="conv3_out") ## --> [3,3,128]
+            
+            self.flatten = tf.layers.flatten(self.conv3_out)
+            ## --> [1152]
+            
+            self.fc = tf.layers.dense(inputs = self.flatten,
+                                  units = 512,
+                                  activation = tf.nn.elu,
+                                  kernel_initializer=tf.contrib.layers.xavier_initializer(),
+                                  name="fc1")
+            self.output = tf.layers.dense(inputs = self.fc, 
+                                          kernel_initializer=tf.contrib.layers.xavier_initializer(),
+                                          units = 3, 
+                                          activation=None)
+            
+            # Q is our predicted Q value.
+            self.Q = tf.reduce_sum(tf.multiply(self.output, self.actions_), axis=1)
+            
+            # The loss is the difference between our predicted Q_values and the Q_target
+            # Sum(Qtarget - Q)^2
+            self.loss = tf.reduce_mean(tf.square(self.target_Q - self.Q))
+            
+            self.optimizer = tf.train.RMSPropOptimizer(self.learning_rate).minimize(self.loss)
+
+
+# Reset the graph
+tf.reset_default_graph()
+
+# Instantiate the DQNetwork
+DQNetwork = DQNetwork(state_size, action_size, learning_rate)
+            
+            
         
